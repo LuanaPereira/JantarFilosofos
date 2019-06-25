078b6945779df0b526e88ac9e04a4f912b5d167b
Luana Pereira <37309259+LuanaPereira@users.noreply.github.com>
2019-06-25 14:37:59 -0300

    Add files via upload

---
+++ JantarFilosofo/JantarFilosofo/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace JantarFilosofo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.iniciar = new System.Windows.Forms.Button();
+            this.Contador1 = new System.Windows.Forms.Label();
+            this.Contador2 = new System.Windows.Forms.Label();
+            this.Contador3 = new System.Windows.Forms.Label();
+            this.Contador4 = new System.Windows.Forms.Label();
+            this.Contador5 = new System.Windows.Forms.Label();
+            this.ListaTela = new System.Windows.Forms.ListBox();
+            this.LFilosofo1 = new System.Windows.Forms.Label();
+            this.LFilosofo2 = new System.Windows.Forms.Label();
+            this.LFilosofo3 = new System.Windows.Forms.Label();
+            this.LFilosofo4 = new System.Windows.Forms.Label();
+            this.LFilosofo5 = new System.Windows.Forms.Label();
+            this.Status1 = new System.Windows.Forms.Label();
+            this.Status2 = new System.Windows.Forms.Label();
+            this.Status3 = new System.Windows.Forms.Label();
+            this.Status4 = new System.Windows.Forms.Label();
+            this.Status5 = new System.Windows.Forms.Label();
+            this.Finalizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // iniciar
+            // 
+            this.iniciar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.iniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iniciar.Location = new System.Drawing.Point(43, 22);
+            this.iniciar.Name = "iniciar";
+            this.iniciar.Size = new System.Drawing.Size(106, 46);
+            this.iniciar.TabIndex = 0;
+            this.iniciar.Text = "INICIAR";
+            this.iniciar.UseVisualStyleBackColor = false;
+            this.iniciar.Click += new System.EventHandler(this.IniciarBotao);
+            // 
+            // Contador1
+            // 
+            this.Contador1.AutoSize = true;
+            this.Contador1.Location = new System.Drawing.Point(40, 190);
+            this.Contador1.Name = "Contador1";
+            this.Contador1.Size = new System.Drawing.Size(35, 13);
+            this.Contador1.TabIndex = 1;
+            this.Contador1.Text = "Cont1";
+            this.Contador1.Click += new System.EventHandler(this.IniciarBotao);
+            // 
+            // Contador2
+            // 
+            this.Contador2.AutoSize = true;
+            this.Contador2.BackColor = System.Drawing.Color.Azure;
+            this.Contador2.Location = new System.Drawing.Point(40, 213);
+            this.Contador2.Name = "Contador2";
+            this.Contador2.Size = new System.Drawing.Size(35, 13);
+            this.Contador2.TabIndex = 2;
+            this.Contador2.Text = "Cont2";
+            this.Contador2.Click += new System.EventHandler(this.IniciarBotao);
+            // 
+            // Contador3
+            // 
+            this.Contador3.AutoSize = true;
+            this.Contador3.Location = new System.Drawing.Point(40, 236);
+            this.Contador3.Name = "Contador3";
+            this.Contador3.Size = new System.Drawing.Size(35, 13);
+            this.Contador3.TabIndex = 3;
+            this.Contador3.Text = "Cont3";
+            // 
+            // Contador4
+            // 
+            this.Contador4.AutoSize = true;
+            this.Contador4.Location = new System.Drawing.Point(40, 258);
+            this.Contador4.Name = "Contador4";
+            this.Contador4.Size = new System.Drawing.Size(35, 13);
+            this.Contador4.TabIndex = 4;
+            this.Contador4.Text = "Cont4";
+            this.Contador4.Click += new System.EventHandler(this.IniciarBotao);
+            // 
+            // Contador5
+            // 
+            this.Contador5.AutoSize = true;
+            this.Contador5.Location = new System.Drawing.Point(37, 282);
+            this.Contador5.Name = "Contador5";
+            this.Contador5.Size = new System.Drawing.Size(38, 13);
+            this.Contador5.TabIndex = 5;
+            this.Contador5.Text = " Cont5";
+            // 
+            // ListaTela
+            // 
+            this.ListaTela.FormattingEnabled = true;
+            this.ListaTela.Location = new System.Drawing.Point(340, 22);
+            this.ListaTela.Name = "ListaTela";
+            this.ListaTela.Size = new System.Drawing.Size(437, 407);
+            this.ListaTela.TabIndex = 6;
+            this.ListaTela.SelectedIndexChanged += new System.EventHandler(this.ListaTela_SelectedIndexChanged);
+            // 
+            // LFilosofo1
+            // 
+            this.LFilosofo1.AutoSize = true;
+            this.LFilosofo1.Location = new System.Drawing.Point(81, 190);
+            this.LFilosofo1.Name = "LFilosofo1";
+            this.LFilosofo1.Size = new System.Drawing.Size(52, 13);
+            this.LFilosofo1.TabIndex = 7;
+            this.LFilosofo1.Text = "Filosofo 1";
+            // 
+            // LFilosofo2
+            // 
+            this.LFilosofo2.AutoSize = true;
+            this.LFilosofo2.Location = new System.Drawing.Point(81, 213);
+            this.LFilosofo2.Name = "LFilosofo2";
+            this.LFilosofo2.Size = new System.Drawing.Size(52, 13);
+            this.LFilosofo2.TabIndex = 8;
+            this.LFilosofo2.Text = "Filosofo 2";
+            // 
+            // LFilosofo3
+            // 
+            this.LFilosofo3.AutoSize = true;
+            this.LFilosofo3.Location = new System.Drawing.Point(81, 236);
+            this.LFilosofo3.Name = "LFilosofo3";
+            this.LFilosofo3.Size = new System.Drawing.Size(52, 13);
+            this.LFilosofo3.TabIndex = 9;
+            this.LFilosofo3.Text = "Filosofo 3";
+            // 
+            // LFilosofo4
+            // 
+            this.LFilosofo4.AutoSize = true;
+            this.LFilosofo4.Location = new System.Drawing.Point(81, 258);
+            this.LFilosofo4.Name = "LFilosofo4";
+            this.LFilosofo4.Size = new System.Drawing.Size(52, 13);
+            this.LFilosofo4.TabIndex = 10;
+            this.LFilosofo4.Text = "Filosofo 4";
+            // 
+            // LFilosofo5
+            // 
+            this.LFilosofo5.AutoSize = true;
+            this.LFilosofo5.Location = new System.Drawing.Point(81, 282);
+            this.LFilosofo5.Name = "LFilosofo5";
+            this.LFilosofo5.Size = new System.Drawing.Size(52, 13);
+            this.LFilosofo5.TabIndex = 11;
+            this.LFilosofo5.Text = "Filosofo 5";
+            // 
+            // Status1
+            // 
+            this.Status1.AutoSize = true;
+            this.Status1.Location = new System.Drawing.Point(140, 190);
+            this.Status1.Name = "Status1";
+            this.Status1.Size = new System.Drawing.Size(46, 13);
+            this.Status1.TabIndex = 12;
+            this.Status1.Text = "Status 1";
+            // 
+            // Status2
+            // 
+            this.Status2.AutoSize = true;
+            this.Status2.Location = new System.Drawing.Point(139, 213);
+            this.Status2.Name = "Status2";
+            this.Status2.Size = new System.Drawing.Size(46, 13);
+            this.Status2.TabIndex = 13;
+            this.Status2.Text = "Status 2";
+            // 
+            // Status3
+            // 
+            this.Status3.AutoSize = true;
+            this.Status3.Location = new System.Drawing.Point(140, 236);
+            this.Status3.Name = "Status3";
+            this.Status3.Size = new System.Drawing.Size(46, 13);
+            this.Status3.TabIndex = 14;
+            this.Status3.Text = "Status 3";
+            // 
+            // Status4
+            // 
+            this.Status4.AutoSize = true;
+            this.Status4.Location = new System.Drawing.Point(140, 258);
+            this.Status4.Name = "Status4";
+            this.Status4.Size = new System.Drawing.Size(46, 13);
+            this.Status4.TabIndex = 15;
+            this.Status4.Text = "Status 4";
+            // 
+            // Status5
+            // 
+            this.Status5.AutoSize = true;
+            this.Status5.Location = new System.Drawing.Point(140, 282);
+            this.Status5.Name = "Status5";
+            this.Status5.Size = new System.Drawing.Size(46, 13);
+            this.Status5.TabIndex = 16;
+            this.Status5.Text = "Status 5";
+            // 
+            // Finalizar
+            // 
+            this.Finalizar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Finalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Finalizar.Location = new System.Drawing.Point(43, 75);
+            this.Finalizar.Name = "Finalizar";
+            this.Finalizar.Size = new System.Drawing.Size(106, 41);
+            this.Finalizar.TabIndex = 17;
+            this.Finalizar.Text = "FINALIZAR";
+            this.Finalizar.UseVisualStyleBackColor = false;
+            this.Finalizar.Click += new System.EventHandler(this.Finalizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Finalizar);
+            this.Controls.Add(this.Status5);
+            this.Controls.Add(this.Status4);
+            this.Controls.Add(this.Status3);
+            this.Controls.Add(this.Status2);
+            this.Controls.Add(this.Status1);
+            this.Controls.Add(this.LFilosofo5);
+            this.Controls.Add(this.LFilosofo4);
+            this.Controls.Add(this.LFilosofo3);
+            this.Controls.Add(this.LFilosofo2);
+            this.Controls.Add(this.LFilosofo1);
+            this.Controls.Add(this.ListaTela);
+            this.Controls.Add(this.Contador5);
+            this.Controls.Add(this.Contador4);
+            this.Controls.Add(this.Contador3);
+            this.Controls.Add(this.Contador2);
+            this.Controls.Add(this.Contador1);
+            this.Controls.Add(this.iniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button iniciar;
+        private System.Windows.Forms.Label Contador1;
+        private System.Windows.Forms.Label Contador2;
+        private System.Windows.Forms.Label Contador3;
+        private System.Windows.Forms.Label Contador4;
+        private System.Windows.Forms.Label Contador5;
+        private System.Windows.Forms.ListBox ListaTela;
+        private System.Windows.Forms.Label LFilosofo1;
+        private System.Windows.Forms.Label LFilosofo2;
+        private System.Windows.Forms.Label LFilosofo3;
+        private System.Windows.Forms.Label LFilosofo4;
+        private System.Windows.Forms.Label LFilosofo5;
+        private System.Windows.Forms.Label Status1;
+        private System.Windows.Forms.Label Status2;
+        private System.Windows.Forms.Label Status3;
+        private System.Windows.Forms.Label Status4;
+        private System.Windows.Forms.Label Status5;
+        private System.Windows.Forms.Button Finalizar;
+    }
+}
+
---
+++ JantarFilosofo/JantarFilosofo/Form1.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JantarFilosofo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random sorteio = new Random();
+        private static Semaphore Semaforo;
+        Thread F1, F2, F3, F4, F5;
+        Garfos[] Garfo = new Garfos[5];
+        Filosofos[] Filosofo = new Filosofos[5];
+        private bool ControleThread = true;
+        private int Controlatempo = 3;
+        private int ContadorFilosofo1 = 0;
+        private int ContadorFilosofo2 = 0;
+        private int ContadorFilosofo3 = 0;
+        private int ContadorFilosofo4 = 0;
+        private int ContadorFilosofo5 = 0;
+
+        private void ListaTela_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Semaforo = new Semaphore(2, 2);
+            F1 = new Thread(Filosofo1);
+            F2 = new Thread(Filosofo2);
+            F3 = new Thread(Filosofo3);
+            F4 = new Thread(Filosofo4);
+            F5 = new Thread(Filosofo5);
+
+            F1.IsBackground = false;
+            F2.IsBackground = false;
+            F3.IsBackground = false;
+            F4.IsBackground = false;
+            F5.IsBackground = false;
+
+            for(int i=0; i<5; i++)
+            {
+                Garfo[i] = new Garfos();
+
+                Filosofo[i] = new Filosofos();
+                Filosofo[i].Nome = "Filosofo " + (i + 1).ToString();
+                
+            }
+
+        }
+        private void IniciarBotao(object sender, EventArgs e)
+        {
+            Contador1.Text = "0";
+            Contador2.Text = "0";
+            Contador3.Text = "0";
+            Contador4.Text = "0";
+            Contador5.Text = "0";
+
+            for (int i=0; i<Garfo.Count(); i++)
+            {
+                Garfo[i].EmUso = false;
+                Filosofo[i].Pensando = false;
+            }
+
+            ControleThread = true;
+
+            F1.Abort();
+            F2.Abort();
+            F3.Abort();
+            F4.Abort();
+            F5.Abort();
+
+            { F1 = new Thread(Filosofo1); F1.Start(); }
+            { F3 = new Thread(Filosofo3); F3.Start(); }
+            { F5 = new Thread(Filosofo5); F5.Start(); }
+            { F2 = new Thread(Filosofo2); F2.Start(); }
+            { F4 = new Thread(Filosofo4); F4.Start(); }
+
+
+            iniciar.Enabled = false;
+            Finalizar.Enabled = true;
+
+        }
+        private void Finalizar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Filosofo1()
+        {
+            while (ControleThread)
+            {
+                if (Garfo[4].EmUso == true || Garfo[0].EmUso == true)
+                {
+                    this.BeginInvoke((MethodInvoker)delegate
+                    {
+                        ListaTela.Items.Add(Filosofo[0].Nome + " está pensando");
+                        Status1.Text = "Pensando";
+
+                    });
+                    Thread.Sleep(2000);
+                }
+                else
+                {
+                    int tempo = sorteio.Next(3000, 6000);
+                    int TempoComendo = tempo / Controlatempo;
+                    Semaforo.WaitOne();
+                    {
+                        Filosofo[0].Pensando = false;
+                        Garfo[0].EmUso = true;
+                        Garfo[4].EmUso = true;
+                       
+                        this.BeginInvoke((MethodInvoker)delegate
+                        {
+                            ListaTela.Items.Add(Filosofo[0].Nome + " está  comendo");
+                            Status1.Text = "Comendo";
+                            ContadorFilosofo1++;
+                           
+                            Contador1.Text = ContadorFilosofo1.ToString();
+                        });
+                      
+
+                        Thread.Sleep(TempoComendo);
+                        this.BeginInvoke((MethodInvoker)delegate
+                        {
+                            ListaTela.Items.Add(Filosofo[0].Nome + " está  pensando");
+                            Status1.Text = "Pensando";
+                        });
+                       
+
+                        Filosofo[0].Pensando = true;
+                        Garfo[0].EmUso = false;
+                        Garfo[4].EmUso = false;
+                      
+
+                    }
+                    Semaforo.Release();
+                    if (ContadorFilosofo1 == 5)
+                    {
+                        F1.Abort();
+                        return;
+                    }
+                }
+            }
+        }
+        private void Filosofo2()
+        {
+            int tempo = sorteio.Next(3000, 6000);
+            int TempoComendo = tempo / Controlatempo;
+            while (ControleThread)
+            {
+                if (Garfo[0].EmUso == true || Garfo[1].EmUso == true)
+                {
+                    this.BeginInvoke((MethodInvoker)delegate
+                    {
+                        ListaTela.Items.Add(Filosofo[1].Nome + " está pensando");
+                        Status2.Text = "Pensando";
+                    });
+                    
+                    Thread.Sleep(2000);
+                }
+                else
+                {
+                    Semaforo.WaitOne();
+                    {
+                        Filosofo[1].Pensando = false;
+                        Garfo[0].EmUso = true;
+                        Garfo[1].EmUso = true;
+
+                        this.BeginInvoke((MethodInvoker)delegate
+                        {
+                            ListaTela.Items.Add(Filosofo[1].Nome + " está  comendo");
+                            Status2.Text = "Comendo";
+                            ContadorFilosofo2++;
+                          
+                            Contador2.Text = ContadorFilosofo2.ToString();
+
+                        });
+                       
+
+                        Thread.Sleep(TempoComendo);
+
+                        this.BeginInvoke((MethodInvoker)delegate
+                        {
+                            ListaTela.Items.Add(Filosofo[1].Nome + " está  pensando");
+                            Status2.Text = "Pensando";
+                        });
+                       
+
+                        Filosofo[1].Pensando = true;
+                        Garfo[0].EmUso = false;
+                        Garfo[1].EmUso = false;
+                       
+                    }
+                    Semaforo.Release();
+                    if (ContadorFilosofo2 == 5)
+                    {
+                        F2.Abort();
+                        return;
+                    }
+                }
+            }
+        }
+        private void Filosofo3()
+        {
+            int tempo = sorteio.Next(3000, 6000);
+            int TempoComendo = tempo / Controlatempo;
+            while (ControleThread)
+            {
+                if (Garfo[1].EmUso == true || Garfo[2].EmUso == true)
+                {
+                    this.BeginInvoke((MethodInvoker)delegate
+                    {
+                        ListaTela.Items.Add(Filosofo[2].Nome + " está pensando");
+                        Status3.Text = "Pensando";
+                    });
+                   
+                    Thread.Sleep(2000);
+                }
+                else
+                {
+                    Semaforo.WaitOne();
+                    {
+                        Filosofo[0].Pensando = false;
+                        Garfo[0].EmUso = true;
+                        Garfo[4].EmUso = true;
+
+                        this.BeginInvoke((MethodInvoker)delegate
+                        {
+                            ListaTela.Items.Add(Filosofo[2].Nome + " está  comendo");
+                            Status3.Text = "Comendo";
+                            ContadorFilosofo3++;
+                            
+                            Contador3.Text = ContadorFilosofo3.ToString();
+                        });
+                       
+
+                        Thread.Sleep(TempoComendo);
+
+                        this.BeginInvoke((MethodInvoker)delegate
+                        {
+                            ListaTela.Items.Add(Filosofo[2].Nome + " está  pensando");
+                            Status3.Text = "Pensando";
+                        });
+                      
+
+                        Filosofo[2].Pensando = true;
+                        Garfo[1].EmUso = false;
+                        Garfo[2].EmUso = false;
+                        
+                    }
+                    Semaforo.Release();
+                    if (ContadorFilosofo3 == 5)
+                    {
+                        F3.Abort();
+                        return;
+                    }
+                }
+            }
+        }
+        private void Filosofo4()
+        {
+            int tempo = sorteio.Next(3000, 6000);
+            int TempoComendo = tempo / Controlatempo;
+            while (ControleThread)
+            {
+                if (Garfo[2].EmUso == true || Garfo[3].EmUso == true)
+                {
+                    this.BeginInvoke((MethodInvoker)delegate
+                    {
+                        ListaTela.Items.Add(Filosofo[3].Nome + " está pensando");
+                        Status4.Text = "Pensando";
+                    });
+                    
+                    Thread.Sleep(2000);
+                }
+                else
+                {
+                    Semaforo.WaitOne();
+                    {
+                        Filosofo[3].Pensando = false;
+                        Garfo[2].EmUso = true;
+                        Garfo[3].EmUso = true;
+                        this.BeginInvoke((MethodInvoker)delegate
+                        {
+                            ListaTela.Items.Add(Filosofo[3].Nome + " está  comendo");
+                            Status4.Text = "Comendo";
+                            ContadorFilosofo4++;
+                           
+                            Contador4.Text = ContadorFilosofo4.ToString();
+                        });
+                       
+                        Thread.Sleep(TempoComendo);
+                        
+                        this.BeginInvoke((MethodInvoker)delegate
+                        {
+                            ListaTela.Items.Add(Filosofo[3].Nome + " está  pensando");
+                            Status4.Text = "Pensando";
+                        });
+                        Filosofo[3].Pensando = true;
+                        Garfo[2].EmUso = false;
+                        Garfo[3].EmUso = false;
+                       
+                    }
+                    Semaforo.Release();
+                    if (ContadorFilosofo4 == 5)
+                    {
+                        F4.Abort();
+                        return;
+                    }
+                }
+            }
+        }
+        private void Filosofo5()
+        {
+            int tempo = sorteio.Next(3000, 6000);
+            int TempoComendo = tempo / Controlatempo;
+            while (ControleThread)
+            {
+                if (Garfo[3].EmUso == true || Garfo[4].EmUso == true)
+                {
+                    this.BeginInvoke((MethodInvoker)delegate
+                    {
+                        ListaTela.Items.Add(Filosofo[4].Nome + " está pensando");
+                        Status5.Text = "Pensando";
+                    });
+                    
+                    Thread.Sleep(2000);
+                }
+                else
+                {
+                    Semaforo.WaitOne();
+                    {
+                        Filosofo[4].Pensando = false;
+                        Garfo[3].EmUso = true;
+                        Garfo[4].EmUso = true;
+
+                        this.BeginInvoke((MethodInvoker)delegate
+                        {
+                            ListaTela.Items.Add(Filosofo[4].Nome + " está  comendo");
+                            Status5.Text = "Comendo";
+                            ContadorFilosofo5++;
+                          
+                            Contador5.Text = ContadorFilosofo5.ToString();
+
+                        });
+                      
+
+                        Thread.Sleep(TempoComendo);
+
+                        this.BeginInvoke((MethodInvoker)delegate
+                        {
+                            ListaTela.Items.Add(Filosofo[4].Nome + " está  pensando");
+                            Status5.Text = "Pensando";
+                        });
+                       
+                        Filosofo[4].Pensando = true;
+                        Garfo[3].EmUso = false;
+                        Garfo[4].EmUso = false;
+                       
+                    }
+                    Semaforo.Release();
+                    if (ContadorFilosofo5 == 5)
+                    {
+                        F5.Abort();
+                        return;
+                    }
+                }
+            }
+        }
+  
+    }
+}
